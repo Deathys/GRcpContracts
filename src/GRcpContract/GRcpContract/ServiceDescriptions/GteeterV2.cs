2d2b28d097f03cfec001dc83cdd34aba4ec379ee
Slava <s.anastasiev@pnlfin.tech>
2023-10-11 14:35:27 +0400

    updated service

---
+++ src/GRcpContract/GRcpContract/ServiceDescriptions/GteeterV2.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using ProtoBuf.Grpc;
+
+namespace GRcpContract.ServiceDescriptions
+{
+    [DataContract]
+    public class HelloReply
+    {
+        [DataMember(Order = 1)]
+        public string Message { get; set; }
+        
+        [DataMember(Order = 2)]
+        public Guid AnswerId { get; set; }
+        
+        [DataMember(Order = 3)]
+        public DateTime Created { get; set; }
+
+        [DataMember(Order = 4)]
+        public decimal Pos { get; set; }
+    }
+
+    [DataContract]
+    public class HelloRequest
+    {
+        [DataMember(Order = 1)]
+        public string Name { get; set; }
+    }
+
+    [ServiceContract]
+    public interface IGreeterV2Service
+    {
+        [OperationContract]
+        Task<HelloReply> SayHelloAsync(HelloRequest request, CallContext context = default);
+    }
+}
